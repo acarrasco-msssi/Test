76ba37134a13d0868e70748ff9e20c28addfdd7c
acarrasco <acarrasco@93801m.sanidad.msc>
2016-01-14 11:19:10 +0100

    Nuevo mensaje

--- test/TestConsola/src/com/msssi/Main.java
+++ test/TestConsola/src/com/msssi/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Esto es un test de consola");
+		System.out.println("Esto es una amplicación al test de consola");
 	}
 }
